0e8fb375025df5a650b63c707c556503168f01eb
Enzo Negri Barizo <contatoenzoegri@outlook.com>
2024-08-12 09:55:40 -0300

    entendendo a estrutura de um projeto react

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {
+          'data/horas em que a pagina foi criada:' 
+          + new Date().toLocaleDateString() + ', ' + new Date().toLocaleTimeString()
+          }
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
