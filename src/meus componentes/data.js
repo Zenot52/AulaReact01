a6c8a69fba049c381ee3f5c1d1658e42207c440f
Zenot521 <contatoenzonegri@outlook.com>
2024-08-19 10:11:26 -0300

    mudancas da aula dia 19 08

--- src/meus componentes/data.js
+++ src/meus componentes/data.js
@@ -7,27 +7,46 @@ export default class Data extends Component{
     constructor(props){
         super(); //executa o constructor pai
         this.props = props;
-        this.dataAtual = new Date().toLocaleString(); 
+        this.state = {
+            dataAtual : new Date().toLocaleString()
+        }
+        
+    }
+
+    pegaDataDe(timeZone){
+        const dataAtual = new Date();
+        let timeZoneFromDB = Number.parseInt(timeZone);
+        let diferencaTempo = timeZoneFromDB * 60 + dataAtual.getTimezoneOffset;
+        return Date(dataAtual.getTime() + diferencaTempo * 60 *1000);
     }
     
     //fase de montagem
     componentDidMount(){
         console.log("O componente foi montado");
-        this.dataAtual = new Date().toLocaleString();
-    // aqui ele tenta atualizar, mas um componente so é atualizado se muda seu estado
-    // ai ele usa o componentDidUpdate()
+        //nao é permitido atualizar um estado do componente de forma direta, tem que usar o setstate
+        this.setState({
+            dataAtual: new Date().toLocaleString()
 
+        })
     }
 
     componentDidUpdate(){
+        //se o componente for atualizado
         console.log("O componente foi atualizado");
-
+        setTimeout(()=>{
+            this.setState({
+                dataAtual: this.pegaDataDe(this.props.timeZone)
+            });
+        },1000);
+        //o loop funciona assim, o comando timeout espera 1sec (1000ms) e entao a partir disso ele executa o 
+        //setstate, quando esse comando vem, ele usa o componentdidupdate, pq esse componente atualizou
+        //ai entao ele chama dnv e dnv
     }
 
     // sobrecrita de metodo
     render(){
         return(
-            <h1>{this.props.texto || ""}{this.dataAtual}</h1>
+            <h1>{this.props.texto || ""}{this.state.dataAtual}</h1>
         )
     }
 
